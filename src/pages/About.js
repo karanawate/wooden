e1068d4dac393c80911a1509b9829a1d56b5a5a4
karanawate <karanawate8@gmail.com>
2022-09-22 22:46:03 +0530

    useMemo using

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,25 @@
-import react from 'react';
+import react, { useMemo } from 'react';
 import { useState } from 'react';
 
 const About  = () =>{
-    const [name, setName] = useState('Karan')
+    const [add, setAdd] = useState(0);
+    const [sub, setSub] = useState(1000);
+
+     const multiplicaton = useMemo(() =>{
+        console.warn('Hello');
+        return add*2;
+     },[add]);
+
     return <div>
-        <h5>Hello {name}</h5>
+      
+        countin addition: <span>{add}</span>
+        <button onClick={()=>setAdd(add + 1)}>Addition</button>
+        <br></br>
+        countin Subtration: <span>{sub}</span>
+        <button onClick={()=>setSub(sub - 1)}>Subtration</button>
+        <br>
+        </br>
+        {multiplicaton}
     </div>
 }
 export default About;
