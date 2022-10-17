3c94049cd056436c94730b62631144c2b850e7bc
karanawate <karanawate8@gmail.com>
2022-10-17 19:49:24 +0530

    Practise useEffect using get data

--- src/pages/Dashbord.js
+++ src/pages/Dashbord.js
@@ -1,6 +1,27 @@
-import react from 'react';
+import axios from 'axios';
+import {useState,useEffect} from 'react';
+
+let usere = localStorage.getItem('user');
 
 const Dashbord = () =>{
-    return <div>Dashbord</div>
+const[isAdmins, setIsAdmins] = useState([])
+
+    useEffect( () =>{
+        loadAdmins()
+    })
+    const loadAdmins = async () =>{
+                        await
+                        axios
+                        .get('http://127.0.0.1:8000/api/users')
+                        .then( res => {
+                            setIsAdmins(res.data.data)
+                        })
+                        .catch(err =>{
+                            console.log(err)
+                        })
+    }
+    return <div>
+        {JSON.stringify(isAdmins)}
+        Dashbord</div>
 }
 export default Dashbord;
